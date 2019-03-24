081934443d75bb8dcf490c5756579c2fa4bbd7e3
Daisuke Kato <kato.daisuke429@gmail.com>
2019-03-24 23:30:58 +0900

    add init_with_level example

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ extern crate simple_logger as logger;
 use log::Level;
 
 fn main() {
-    logger::init().unwrap();
+    // logger::init().unwrap();
+    logger::init_with_level(Level::Info).unwrap();
 
     debug!("this is a debug {}", "message");
     error!("this is printed by default");
